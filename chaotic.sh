d4a3674d95b437b7cf61de93e05bcf04785b05b4
notharib <drawwithharib@gmail.com>
2026-01-27 17:58:33 +0000

    chaotic fix

--- chaotic.sh
+++ chaotic.sh
@@ -6,7 +6,7 @@ sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
-sudo echo "[chaotic-aur]" >>/etc/pacman.conf
-sudo echo "Include = /etc/pacman.d/chaotic-mirrorlist" >>/etc/pacman.conf
+echo "[chaotic-aur]" | sudo tee -a /etc/pacman.conf
+echo "Include = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
 
 sudo pacman -Syu
